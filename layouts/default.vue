afb11457315849dc23a6cb3095ab3a5fe2b57348
kema-ray <rachel.oyondi@roamtech.com>
2025-02-26 13:19:15 +0300

    added a default layout and routing

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="min-h-screen bg-gray-100">
+        <!-- <AppHeader /> -->
+        <main class="container mx-auto py-6 px-4">
+            <slot />
+        </main>
+        <footer class="bg-white py-4 borrder-t">
+            <div class="container mx-auto text-center text-gray-500">
+                &copy; {{ new Date().getFullYear() }} Budget App
+            </div>
+        </footer>
+    </div>
+</template>
