ca11e4542ffe1aae540f991ebcc63532630975e7
kema-ray <rachel.oyondi@roamtech.com>
2025-02-26 13:59:45 +0300

    created reusable appbutton, appcard and appmodal components

---
+++ components/ui/AppCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-white rounded-lg shadow-md overflow-hidden">
+        <div v-if="$slots.header" class="border-b border-gray-200 px-4 py-3">
+            <slot name="header" />
+        </div>
+        <div>
+            <slot />
+        </div>
+        <div v-if="$slots.footer" class="border-t border-gray-200 px-4 py-3">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
