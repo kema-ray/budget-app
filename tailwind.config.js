ed1d1b69fa3d7ccdf86eb4d331510f70bf50ae6a
kema-ray <rachel.oyondi@roamtech.com>
2025-02-26 13:09:05 +0300

    Tailwind CSS Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4F46E5',
+        secondary: '#10B981',
+        danger: '#EF4444',
+        warning: '#F59E0B',
+        info: '#3B82F6',
+      }
+    },
   },
   plugins: [],
 }
