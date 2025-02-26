9ed39c2d22c1b83016dbc08b67bf4f85dc9f3ad4
kema-ray <rachel.oyondi@roamtech.com>
2025-02-26 13:07:35 +0300

    adding configurations for tailwind css and pinia to nuxt.config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,26 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  // postcss: {
+  //   plugins: {
+  //     tailwindcss: {},
+  //     autoprefixer: {},
+  //   },
+  // },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
+  css: [
+    '@/assets/css/main.css',
+  ],
+  app: {
+    head: {
+      title: 'Budget App',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    }
+  }
 })
